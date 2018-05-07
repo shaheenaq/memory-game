75e313c3cac7daaf7361ed51c518968c477d89bd
Shaheena Qureshi <shaheenaqureshi@ShaheenasMBPro.hsd1.ga.comcast.net>
2018-05-07 18:53:04 -0400

    adds initial files

---
+++ game.js
@@ -0,0 +1,24 @@
+const imgArray = [{
+	'name': 'watermelon',
+	'img': './images/watermelon.png'
+},
+{
+	'name': 'kiwi',
+	'img': './images/kiwi.png'
+},
+{
+	'name': 'fruit',
+	'img': './images/fruitheart.png'
+},
+{
+	'name': 'pineapple',
+	'img': './images/pineapple.png'
+},
+{
+	'name': 'grapes',
+	'img': './images/grapes.png'
+},
+{
+	'name': 'butterfly',
+	'img': './images/orangebutterfly.png'
+}];
